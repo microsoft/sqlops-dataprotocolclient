0c63ede8ac63356cede6a8a64740a46bb8630c34
Anthony Dresser <andresse@microsoft.com>
2018-01-02 14:44:01 -0800

    update

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,10 +1,25 @@
-import { LanguageClient, ServerOptions, LanguageClientOptions as VSLanguageClientOptions } from 'vscode-languageclient';
+import { LanguageClient, ServerOptions, LanguageClientOptions as VSLanguageClientOptions, DynamicFeature, ServerCapabilities, RegistrationData, RPCMessageType, Disposable } from 'vscode-languageclient';
 import { Ic2p } from './codeConverter';
+import * as protocol from './protocol';
 import { Ip2c } from './protocolConverter';
-export * from './features';
 export interface LanguageClientOptions extends VSLanguageClientOptions {
     providerId: string;
-    serverConnectionMetadata: any;
+}
+/**
+ *
+ */
+export declare abstract class SqlOpsFeature<T> implements DynamicFeature<T> {
+    protected _client: SqlOpsDataClient;
+    private _message;
+    protected _providers: Map<string, Disposable>;
+    constructor(_client: SqlOpsDataClient, _message: RPCMessageType | RPCMessageType[]);
+    readonly messages: RPCMessageType | RPCMessageType[];
+    abstract fillClientCapabilities(capabilities: protocol.ClientCapabilities): void;
+    abstract initialize(capabilities: ServerCapabilities): void;
+    register(messages: RPCMessageType | RPCMessageType[], data: RegistrationData<T>): void;
+    protected abstract registerProvider(options: T): Disposable;
+    unregister(id: string): void;
+    dispose(): void;
 }
 /**
  *
@@ -18,4 +33,5 @@ export declare class SqlOpsDataClient extends LanguageClient {
     readonly providerId: string;
     constructor(name: string, serverOptions: ServerOptions, clientOptions: LanguageClientOptions, forceDebug?: boolean);
     constructor(id: string, name: string, serverOptions: ServerOptions, clientOptions: LanguageClientOptions, forceDebug?: boolean);
+    private registerDataFeatures();
 }
