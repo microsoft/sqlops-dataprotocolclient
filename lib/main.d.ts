6714426de8e1c0788a4e26b15721c44f32227e2f
Anthony Dresser <andresse@microsoft.com>
2018-03-15 10:48:49 -0700

    release

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -7,7 +7,6 @@ export interface ISqlOpsFeature {
 }
 export interface ClientOptions extends VSLanguageClientOptions {
     providerId: string;
-    serverConnectionMetadata: any;
     features?: Array<ISqlOpsFeature>;
 }
 /**
