0dbd5a6fa7fbe6de5feb7718fa59341f2e5e3315
Karl Burtram <karlb@microsoft.com>
2018-08-01 15:16:29 -0400

    Add getConnectionString method to the Connection feature api (#17)
    * Add GetConnectionString method
    
    * Add include password parameter

--- src/main.ts
+++ src/main.ts
@@ -225,6 +225,21 @@ export class ConnectionFeature extends SqlOpsFeature<undefined> {
 			);
 		};
 
+		let getConnectionString = (ownerUri: string, includePassword: boolean): Thenable<string> => {
+			let params: protocol.GetConnectionStringParams = {
+				ownerUri,
+				includePassword
+			};
+
+			return client.sendRequest(protocol.GetConnectionStringRequest.type, params).then(
+				r => r,
+				e => {
+					client.logFailedRequest(protocol.GetConnectionStringRequest.type, e);
+					return Promise.resolve(undefined);
+				}
+			);
+		};
+
 		let rebuildIntelliSenseCache = (ownerUri: string): Thenable<void> => {
 			let params: protocol.RebuildIntelliSenseParams = {
 				ownerUri
@@ -260,6 +275,7 @@ export class ConnectionFeature extends SqlOpsFeature<undefined> {
 			cancelConnect,
 			changeDatabase,
 			listDatabases,
+			getConnectionString,
 			rebuildIntelliSenseCache,
 			registerOnConnectionChanged,
 			registerOnIntelliSenseCacheComplete,
