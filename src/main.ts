0453f4b28e933a823656a921e9f908cc2f4074e3
Alan Ren <alanren@microsoft.com>
2022-11-04 11:22:02 -0700

    throw the scripting error (#69)

--- src/main.ts
+++ src/main.ts
@@ -1122,7 +1122,7 @@ export class ScriptingFeature extends SqlOpsFeature<undefined> {
 					r => r,
 					e => {
 						client.logFailedRequest(protocol.ScriptingRequest.type, e);
-						return Promise.resolve(undefined);
+						return Promise.reject(e);
 					}
 				);
 		};
